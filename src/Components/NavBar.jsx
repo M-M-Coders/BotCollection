b0ee4e3c8647312ad00b351ff70e29cdc308d347
clairemuiru1 <muiruclaire@gmail.com>
2023-11-04 10:46:35 +0300

    "make changes"

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "wheat",
+};
+
+
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div>
+      <a href='home' style={linkStyles}> Collection</a>
+      <a href='home' style={linkStyles}>  Army</a>
+    </div>
   )
 }
 
