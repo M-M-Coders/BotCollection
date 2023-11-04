1afd5bd9ac966323a4bb67a28d5a02c44e356fe4
clairemuiru1 <muiruclaire@gmail.com>
2023-11-04 13:28:48 +0300

    "Fetched bots"

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './NavBar'
-// import './App.css'
+import BotList from './BotList'
+import './App.css'
 
 function App() {
   return (
     <div className='container'>
-      App
+      <h1> Bot Realm X </h1>
       <NavBar className='nav'/>
+      <BotList/>
     </div>
   )
 }
---
+++ src/Components/BotList.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+
+function BotList() {
+    const [bots, setBots] = useState([]);
+
+    useEffect(() => {
+        async function fetchBots() {
+            const response = await fetch("http://localhost:3000/bots")
+            const bots = await response.json()
+            setBots(bots)
+        }
+        fetchBots()
+    }, [])
+    return (
+        <div>
+            {bots.map((bots, index) => {
+                return (
+                    <div key={index}>
+                        <img src={bots.avatar_url}/>
+                        <p>Name:{bots.name}</p>
+                        <p>Health:{bots.health}</p>
+                        <p>Damage:{bots.damage}</p>
+                        <p>Armor:{bots.armor}</p>
+                        <p>Bot_Class:{bots.bot_class}</p>
+                        <p>Phrase:{bots.catchphrase}</p>
+                        <p>Created_at:{bots.created_at}</p>
+                        <p>Updated_at:{bots.updated_at}</p>
+                        <button className='btn'>X</button> 
+                        <button>Add to Army</button>
+                    </div>
+                    )
+            })}
+        </div>
+    )
+}
+
+export default BotList
